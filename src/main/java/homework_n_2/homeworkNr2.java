dd0b897a65f37c7c26abd61c850feb941c44f075
teserax <steeles80@gmail.com>
2023-09-07 00:05:18 +0300

    made homework_nr2

---
+++ src/main/java/homework_n_2/homeworkNr2.java
@@ -0,0 +1,32 @@
+package homework_n_2;
+public class homeworkNr2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        long CurrentWorldPopulation= 7811080815L;
+        int CarsProducedThisYear =55235358;
+        char FirstLetterInAlphabet= 'A';
+        boolean JavaIsAmazing=true;
+        float Pi=3.14159F;
+
+        System.out.println(CurrentWorldPopulation);
+        System.out.println(CarsProducedThisYear);
+        System.out.println(FirstLetterInAlphabet);
+        System.out.println(JavaIsAmazing);
+        System.out.println(Pi);
+
+        double Price =54.2345F;
+        double Tax =33.478F;
+        int  Quantity=52;
+        double sum =0;
+
+        System.out.println(Price);
+        System.out.println(Tax);
+        System.out.println(Quantity);
+        System.out.println(sum);
+
+        double total = Price + Tax * Quantity + sum;
+
+        System.out.println(sum);
+
+    }
+}
