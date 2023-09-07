67d35f76d68d226c9226b287c9313450e87ddf78
TESERAC <steeles80@gmail.com>
2023-09-07 10:03:49 +0300

    add-on fixes

--- src/main/java/homework_n_2/homeworkNr2.java
+++ src/main/java/homework_n_2/homeworkNr2.java
@@ -1,7 +1,9 @@
 package homework_n_2;
 public class homeworkNr2 {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+
         long CurrentWorldPopulation= 7811080815L;
         int CarsProducedThisYear =55235358;
         char FirstLetterInAlphabet= 'A';
@@ -26,7 +28,7 @@ public class homeworkNr2 {
 
         double total = Price + Tax * Quantity + sum;
 
-        System.out.println(sum);
+        System.out.println(total);
 
     }
 }
