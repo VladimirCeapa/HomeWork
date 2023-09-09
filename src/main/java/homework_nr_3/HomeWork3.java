e8b52451e56aa742a4c8cbeead5e4fcb40c4b694
teserax <steeles80@gmail.com>
2023-09-09 10:52:54 +0300

    homework_nr3

---
+++ src/main/java/homework_nr_3/HomeWork3.java
@@ -0,0 +1,65 @@
+package homework_nr_3;
+
+public class HomeWork3 {
+
+    public static void main(String[] args) {
+
+        int month = 3;
+        switch (month) {
+            case 1:
+                System.out.println("Январь");
+                break;
+            case 2:
+                System.out.println("Февраль");
+                break;
+            case 3:
+                System.out.println("Март");
+                break;
+            case 4:
+                System.out.println("Апрель");
+                break;
+            case 5:
+                System.out.println("Май");
+                break;
+            case 6:
+                System.out.println("Июнь");
+                break;
+            case 7:
+                System.out.println("Июль");
+                break;
+            case 8:
+                System.out.println("Август");
+                break;
+            case 9:
+                System.out.println("Сентябрь");
+                break;
+            case 10:
+                System.out.println("Октябрь");
+                break;
+            case 11:
+                System.out.println("Ноябрь");
+                break;
+            case 12:
+                System.out.println("Декабрь");
+                break;
+
+
+        }
+        for (int i = 100; i <= 1000; i++) {
+
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        float total = 0;
+        int t = 3;
+        for (float i = 1; i < 100; i += 2) {
+            total = (i / t);
+            t += i;
+        }
+        System.out.println("total:= " + total);
+    }
+
+
+}
