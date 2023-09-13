5fa0b052e71c3c3ffc471165f6ee82133def10f1
TESERAC <steeles80@gmail.com>
2023-09-13 20:39:50 +0300

    corrections home work nr 4

--- src/main/java/homework_nr_4/Circle.java
+++ src/main/java/homework_nr_4/Circle.java
@@ -3,21 +3,20 @@ package homework_nr_4;
 public class Circle {
 
     public static void main(String[] args) {
-        int radius = 4;
-        calculateArea(radius);
+
         Circle circle1 = new Circle(10);
-        calculateArea(circle1.radius);
+        circle1.calculateArea();
         TemperatureConverter converter = new TemperatureConverter(55,578);
         System.out.println("Convert Fahrenheit to Celsius:"+converter.toCelsius(converter.fahrenheit));
         System.out.println("Convert Celsius to Fahrenheit:"+converter.toFahrenheit(converter.celsius));
     }
-
-    public static void calculateArea(int radius) {
+    float radius;
+    public  void calculateArea() {
 
         System.out.println("Radius:="+Math.PI * (radius * radius));
     }
 
-    int radius;
+
 
     public Circle(int radius) {
         this.radius = radius;
