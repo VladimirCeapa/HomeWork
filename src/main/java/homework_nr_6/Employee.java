b6fce183d1e3788c0365fc751111239888e03d23
TESERAC <steeles80@gmail.com>
2023-09-20 17:32:39 +0300

    homework_nr_6 1.1

--- src/main/java/homework_nr_6/Employee.java
+++ src/main/java/homework_nr_6/Employee.java
@@ -1,4 +1,37 @@
 package homework_nr_6;
 
 public class Employee {
+    private String name;
+    private int age;
+    private String Department;
+
+    public Employee(String name, int age, String department) {
+        this.name = name;
+        this.age = age;
+        Department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getDepartment() {
+        return Department;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDepartment(String department) {
+        Department = department;
+    }
 }
---
+++ src/main/java/homework_nr_6/main.java
@@ -0,0 +1,11 @@
+package homework_nr_6;
+
+public class main {
+    public static void main(String[] args) {
+Invoice payment1=new Invoice("","",-1,0);
+        System.out.println(payment1.getAmount());
+
+
+
+    }
+}
