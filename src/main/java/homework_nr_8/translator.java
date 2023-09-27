44618423f0d7b0ee4f4fd3d20fef9dc87f36384e
TESERAC <steeles80@gmail.com>
2023-09-27 10:49:34 +0300

    homework_nr_8

---
+++ src/main/java/homework_nr_8/translator.java
@@ -0,0 +1,36 @@
+package homework_nr_8;
+
+import com.sun.jdi.Value;
+
+import java.security.Key;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static homework_nr_8.main.line;
+
+public class translator {
+
+    public static void translation(HashMap<String, String> input, String word) {
+        boolean key = false;
+        key = input.containsKey(word);
+        if (input.get(word) != null && key) {
+
+            System.out.println("traducere din engleză în română  " + word + "-" + input.get(word));
+            line();
+        }
+        if (!key && input.get(word) == null) {
+            for (String k : input.keySet()) {
+                if (word.equals(input.get(k))) {
+                    System.out.println("traducere din română în engleză " + word + "-" + k);
+                    line();
+                }
+            }
+        }else {
+            System.out.println("Din pacate nu putem traduce");
+            line();
+        }
+
+    }
+
+}
