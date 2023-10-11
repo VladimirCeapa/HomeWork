f34dea1cd754e7fa926f6b94c8dca6c5f4ad7275
TESERAC <steeles80@gmail.com>
2023-10-11 17:33:10 +0300

    home work

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,12 +1,23 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
-class test{
+class test {
     public static void main(String[] args) {
-         int  [] s ={1,2,3,4,5,6,7,8,9};
-         for(int si:s){
-             System.out.println(si);
-         }
-        System.out.println(Arrays.toString(s));
+        String s = "ae@face.md; ser@mail.ru; ww@gmail.com;";
+test t =new test();
+test.email(s);
+    }
+
+    public static void email(String s) {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        while (c < s.length()-1) {
+            //ae@face.md; ser@mail.ru; ww@gmail.com;
+            a = s.indexOf('@', c);
+            b = s.indexOf('.', c);
+            c = s.indexOf(';', c + 1);
+            System.out.println(s.substring(a+1,b));
+        }
     }
 }
