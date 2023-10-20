405399fa9788abaf0ad5236500630cacc2bb5bba
teserax <steeles80@gmail.com>
2023-10-20 22:55:13 +0300

    homework 12

---
+++ src/main/java/homework_nr_12/Months.java
@@ -0,0 +1,17 @@
+package homework_nr_12;
+
+public enum Months {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULI,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+
+}
