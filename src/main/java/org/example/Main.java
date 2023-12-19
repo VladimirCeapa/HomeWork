2870c0b112e51b24ab9abff8e346355a7d9476b9
TESERAC <steeles80@gmail.com>
2023-12-19 17:43:37 +0200

    work game Sweeper

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello world!2");
+=======
+        System.out.println("Hello world!");
+>>>>>>> b655d77 (work game Sweeper)
     }
 }
