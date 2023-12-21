989c03ca1ad20a8fba97c68c547dec7ed2c55819
TESERAC <steeles80@gmail.com>
2023-12-21 19:03:09 +0200

    work game Sweeper

--- src/main/java/JavaSweeper.java
+++ src/main/java/JavaSweeper.java
@@ -1,21 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
-
+import sweeper.Box;
 public class JavaSweeper extends JFrame {
+    private final int COLS =15;
+    private final int ROWS=1;
+    private final int IMAGE_SIZE=50;
     private JPanel panel;
 
     public static void main(String[] args) {
-        new JavaSweeper().setVisible(true);
+        new JavaSweeper();
     }
 
     private JavaSweeper() {
+        setImages();
         InitPanel();
         InitFrame();
     }
 
     private void InitPanel() {
-        panel = new JPanel();//create new panel
-        panel.setPreferredSize(new Dimension(500, 300));//size of panel
+        panel = new JPanel(){
+            @Override
+            protected  void paintComponent(Graphics g){
+                super.paintComponent(g);
+                for (Box box:Box.values()){
+                    g.drawImage((Image) box.image,
+                            box.ordinal()*IMAGE_SIZE,
+                            0,this);
+                }
+            }
+        };//create new panel
+        panel.setPreferredSize(new Dimension(COLS*IMAGE_SIZE, ROWS*IMAGE_SIZE));//size of panel
         add(panel);//add
     }
     private void InitFrame() {
@@ -26,5 +40,14 @@ public class JavaSweeper extends JFrame {
         setResizable(false);//make format close
         pack();//rar in pack
         setLocationRelativeTo(null);//Center of center
+    }
+    private void setImages(){
+        for(Box box:Box.values())
+            box.image=getImage(box.name().toLowerCase());
+    }
+    private Image getImage(String name){
+        String filename="res/img/"+name+".png";
+        ImageIcon icon =new javax.swing.ImageIcon(filename);
+        return icon.getImage();
     }
 }
---
+++ src/main/java/sweeper/Box.java
@@ -0,0 +1,24 @@
+package sweeper;
+
+import java.awt.*;
+
+public enum Box {
+    ZERO,
+    NUM1,
+    NUM2,
+    NUM3,
+    NUM4,
+    NUM5,
+    NUM6,
+    NUM7,
+    NUM8,
+    NUM9,
+
+    OPENED,
+    CLOSED,
+    FLAGED,
+    BOMBED,
+    NOBOMB;
+    public Object image;
+
+}
